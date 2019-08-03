e57d426bc89ed8c3362ce13c69ef7e699a7e175e
Toshihiro NAKAE <tnakae@users.noreply.github.com>
2019-08-04 01:00:21 +0900

    Added map files for navigation

--- catkin_ws/src/scripts/test_navigation.sh
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 source ../../devel/setup.bash
-MAPFILE=`realpath ../map/simple_walls.world`
+TURTLEBOT_GAZEBO_WORLD_FILE=`realpath ../map/simple_walls.world`
+TURTLEBOT_GAZEBO_MAP_FILE=`realpath ../map/map.yaml`
 
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${MAPFILE}" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
