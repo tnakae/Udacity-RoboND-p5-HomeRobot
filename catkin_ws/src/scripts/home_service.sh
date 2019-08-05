aecf787ad9d7ff64ed127e895d98d3801019d736
Toshihiro NAKAE <tnakae@users.noreply.github.com>
2019-08-06 00:00:34 +0900

    Changed to environmental variables

--- catkin_ws/src/scripts/home_service.sh
+++ catkin_ws/src/scripts/home_service.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 source ../../devel/setup.bash
-TURTLEBOT_GAZEBO_WORLD_FILE=`realpath ../map/simple_walls.world`
-TURTLEBOT_GAZEBO_MAP_FILE=`realpath ../map/map.yaml`
-TURTLEBOT_CUSTOM_PARAM_FILE=`realpath ../turtlebot_simulator/turtlebot_gazebo/config/custom_cost_params.yaml`
+export TURTLEBOT_GAZEBO_WORLD_FILE=`realpath ../map/simple_walls.world`
+export TURTLEBOT_GAZEBO_MAP_FILE=`realpath ../map/map.yaml`
+export TURTLEBOT_CUSTOM_PARAM_FILE=`realpath ../turtlebot_simulator/turtlebot_gazebo/config/custom_cost_params.yaml`
 
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
